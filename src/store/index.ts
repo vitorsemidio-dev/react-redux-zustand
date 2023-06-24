7cc24d3b026708c7523f9b33eeec8dfe34c302a4
Vitor Emidio <vitorsemidio.96@gmail.com>
2023-06-24 04:59:58 -0300

    chore: remove example

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,28 +1,9 @@
-import { configureStore, createSlice } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import { TypedUseSelectorHook, useSelector } from 'react-redux';
 
-const todoSlide = createSlice({
-  name: 'todo',
-  initialState: [
-    'Learn React',
-    'Learn TypeScript',
-    'Learn Zustand',
-    'Learn Redux Toolkit',
-  ],
-  reducers: {
-    add: (state, action) => {
-      state.push(action.payload.newTodo);
-    },
-  },
-});
-
 export const store = configureStore({
-  reducer: {
-    todo: todoSlide.reducer,
-  },
+  reducer: {},
 });
-
-export const { add } = todoSlide.actions;
 
 type RootState = ReturnType<typeof store.getState>;
 
