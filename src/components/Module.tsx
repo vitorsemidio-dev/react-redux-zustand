5088cd15c06abd59a89729ad715b43d9bb7ad024
Vitor Emídio <vitorsemidio.96@gmail.com>
2023-06-24 17:08:54 -0300

    feat: collapsible modules

--- src/components/Module.tsx
+++ src/components/Module.tsx
@@ -1,3 +1,4 @@
+import * as Collapsible from "@radix-ui/react-collapsible";
 import { ChevronDown } from "lucide-react";
 
 import { Lesson } from "./Lesson";
@@ -10,27 +11,25 @@ interface ModuleProps {
 
 export function Module({ amountOfLessons, moduleIndex, title }: ModuleProps) {
   return (
-    <div>
-      <button className="flex w-full items-center gap-3 bg-zinc-800 p-4">
+    <Collapsible.Root className="group">
+      <Collapsible.Trigger className="flex w-full items-center gap-3 bg-zinc-800 p-4">
         <div className="flex h-10 w-10 items-center justify-center rounded-full bg-zinc-950 text-xs">
           {moduleIndex + 1}
         </div>
         <div className="flex flex-col gap-1 text-left">
-          <strong className="text-sm ">
-            {title}
-          </strong>
-          <span className="text-xs text-zinc-400">
-            {amountOfLessons} aulas
-          </span>
+          <strong className="text-sm ">{title}</strong>
+          <span className="text-xs text-zinc-400">{amountOfLessons} aulas</span>
         </div>
-        <ChevronDown className="ml-auto h-4 w-4 text-zinc-400" />
-      </button>
+        <ChevronDown className="ml-auto h-4 w-4 text-zinc-400 group-data-[state=open]:rotate-180 transition-transform" />
+      </Collapsible.Trigger>
 
-      <nav className="relative flex flex-col gap-4 p-6">
-        <Lesson title="Fundamentos do Redux" duration="5:06" />
-        <Lesson title="Fundamentos do Redux" duration="5:06" />
-        <Lesson title="Fundamentos do Redux" duration="5:06" />
-      </nav>
-    </div>
+      <Collapsible.Content>
+        <nav className="relative flex flex-col gap-4 p-6">
+          <Lesson title="Fundamentos do Redux" duration="5:06" />
+          <Lesson title="Fundamentos do Redux" duration="5:06" />
+          <Lesson title="Fundamentos do Redux" duration="5:06" />
+        </nav>
+      </Collapsible.Content>
+    </Collapsible.Root>
   );
 }
