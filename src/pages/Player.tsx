b4c4df32188f501eb87ee0149012df8114ce8e4f
Vitor Emidio <vitorsemidio.96@gmail.com>
2023-06-24 05:27:40 -0300

    feat: create module section

--- src/pages/Player.tsx
+++ src/pages/Player.tsx
@@ -1,24 +1,37 @@
-import { MessageCircle } from 'lucide-react';
+import { ChevronDown, MessageCircle } from 'lucide-react';
 
 export function Player() {
   return (
-    <div className="h-screen bg-zinc-950 text-zinc-50 flex justify-center items-center">
+    <div className="flex h-screen items-center justify-center bg-zinc-950 text-zinc-50">
       <div className="flex w-[1100px] flex-col gap-6">
         <div className="flex items-center justify-between">
           <div className="flex flex-col gap-1">
             <h1 className="text-2xl font-bold">Fundamentos do Redux</h1>
-            <span className="text-sm zinc-400">Módulo Redux</span>
+            <span className="zinc-400 text-sm">Módulo Redux</span>
           </div>
 
           <button className="flex items-center gap-2 rounded bg-violet-500 px-3 py-2 text-sm font-medium text-white hover:bg-violet-600">
-            <MessageCircle className="w-4 h-4" />
+            <MessageCircle className="h-4 w-4" />
             Deixar Feedback
           </button>
         </div>
 
         <main className="relative flex overflow-hidden rounded-lg border border-zinc-800 bg-zinc-900 shadow">
           <div className="flex-1">Video</div>
-          <aside className="w-80 border-l border-zinc-800 bg-zinc-900 h-[600px]"></aside>
+          <aside className="h-[600px] w-80 border-l border-zinc-800 bg-zinc-900">
+            <div>
+              <button className="flex w-full items-center gap-3 bg-zinc-800 p-4">
+                <div className="flex h-10 w-10 items-center justify-center rounded-full bg-zinc-950 text-xs">
+                  1
+                </div>
+                <div className="flex flex-col gap-1 text-left">
+                  <strong className="text-sm ">Descobrindo o Redux</strong>
+                  <span className="text-xs text-zinc-400">12 aulas</span>
+                </div>
+                <ChevronDown className="ml-auto h-4 w-4 text-zinc-400" />
+              </button>
+            </div>
+          </aside>
         </main>
       </div>
     </div>
