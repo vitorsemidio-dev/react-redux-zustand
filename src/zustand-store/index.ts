82eabbc0089a53dea7bdc485ef0c465b75413434
Vitor Emidio <vitorsemidio.96@gmail.com>
2023-06-25 05:16:20 -0300

    feat: setup zustand

---
+++ src/zustand-store/index.ts
@@ -0,0 +1,63 @@
+import { create } from 'zustand';
+
+interface Course {
+  id: number;
+  modules: Array<{
+    id: number;
+    title: string;
+    lessons: Array<{
+      id: string;
+      title: string;
+      duration: string;
+    }>;
+  }>;
+}
+
+type PlayParams = {
+  lessonIndex: number;
+  moduleIndex: number;
+};
+
+export interface PlayerState {
+  course: Course | null;
+  currentModuleIndex: number;
+  currentLessonIndex: number;
+  isLoading: boolean;
+  next: () => void;
+  play: (params: PlayParams) => void;
+}
+
+export const useStore = create<PlayerState>((set, get) => ({
+  course: null,
+  currentModuleIndex: 0,
+  currentLessonIndex: 0,
+  isLoading: true,
+  next: () => {
+    const { course, currentLessonIndex, currentModuleIndex } = get();
+    const nextLessonIndex = currentLessonIndex + 1;
+    const nextLesson =
+      course?.modules[currentModuleIndex].lessons[nextLessonIndex];
+
+    if (nextLesson) {
+      set({
+        currentLessonIndex: nextLessonIndex,
+      });
+    } else {
+      const START_INDEX = 0;
+      const nextModuleIndex = currentModuleIndex + 1;
+      const nextModule = course?.modules[nextModuleIndex];
+      if (nextModule) {
+        set({
+          currentLessonIndex: START_INDEX,
+          currentModuleIndex: nextModuleIndex,
+        });
+      }
+    }
+  },
+  play: ({ moduleIndex, lessonIndex }: PlayParams) => {
+    set({
+      currentModuleIndex: moduleIndex,
+      currentLessonIndex: lessonIndex,
+    });
+  },
+}));
