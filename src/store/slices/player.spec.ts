63c306b75caf604072663f3473969c92860028e5
Vitor Emidio <vitorsemidio.96@gmail.com>
2023-06-24 21:48:37 -0300

    test: add tests for action next

--- src/store/slices/player.spec.ts
+++ src/store/slices/player.spec.ts
@@ -2,14 +2,70 @@ import { describe, expect, it } from 'vitest';
 
 import { playerSlice, player as reducer } from './player';
 
+const exampleState = {
+  course: {
+    modules: [
+      {
+        id: '1',
+        title: 'Iniciando com React',
+        lessons: [
+          { id: 'Jai8w6K_GnY', title: 'CSS Modules', duration: '13:45' },
+          { id: 'w-DW4DhDfcw', title: 'Estilização do Post', duration: '10:05' },
+        ],
+      },
+      {
+        id: '2',
+        title: 'Estrutura da aplicação',
+        lessons: [
+          { id: 'gE48FQXRZ_o', title: 'Componente: Comment', duration: '13:45' },
+          { id: 'Ng_Vk4tBl0g', title: 'Responsividade', duration: '10:05' },
+        ],
+      },
+    ],
+  },
+  currentModuleIndex: 0,
+  currentLessonIndex: 0,
+}
+
 describe('player slice', () => {
-  it('should be able to play', () => {
-    const initialState = playerSlice.getInitialState();
-    const input = { moduleIndex: 1, lessonIndex: 2 };
+  describe('Action play', () => {
+    it('should be able to play specific lesson', () => {
+      const input = { moduleIndex: 1, lessonIndex: 2 };
+  
+      const state = reducer(exampleState, playerSlice.actions.play(input));
+  
+      expect(state.currentModuleIndex).toBe(1);
+      expect(state.currentLessonIndex).toBe(2);
+    });
+  });
 
-    const state = reducer(initialState, playerSlice.actions.play(input));
-
-    expect(state.currentModuleIndex).toBe(1);
-    expect(state.currentLessonIndex).toBe(2);
-  })
+  describe('Action next', () => {
+    it('should be able to play next lesson', () => {
+      const state = reducer(exampleState, playerSlice.actions.next());
+  
+      expect(state.currentModuleIndex).toBe(0);
+      expect(state.currentLessonIndex).toBe(1);
+    });
+  
+    it('should be able to play next module', () => {
+      const state = reducer({ ...exampleState, currentLessonIndex: 1 }, playerSlice.actions.next());
+  
+      expect(state.currentModuleIndex).toBe(1);
+      expect(state.currentLessonIndex).toBe(0);
+    });
+  
+    it('should be able to play next lesson when current lesson is the last one', () => {
+      const state = reducer({ ...exampleState, currentLessonIndex: 1 }, playerSlice.actions.next());
+  
+      expect(state.currentModuleIndex).toBe(1);
+      expect(state.currentLessonIndex).toBe(0);
+    });
+  
+    it('should not update current lesson and module indexes when the the lesson and module are the last one', () => {
+      const state = reducer({ ...exampleState, currentLessonIndex: 1, currentModuleIndex: 1 }, playerSlice.actions.next());
+  
+      expect(state.currentModuleIndex).toBe(1);
+      expect(state.currentLessonIndex).toBe(1);
+    });
+  });
 })
